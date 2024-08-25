e1f9d748cbca84328536dae2ef3d3652a13959ab
rishinal <nallasrh@mail.uc.edu>
2024-08-25 18:02:55 -0400

    updated settings.py with two lines about Static files

--- blog/settings.py
+++ blog/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"] # new
+STATIC_ROOT = BASE_DIR / "staticfiles" # new
 MEDIA_URL= '/media/'
 MEDIA_ROOT= BASE_DIR / 'media/'
 # Default primary key field type
